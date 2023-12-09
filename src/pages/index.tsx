bed4db026c9f048a9dedf55295603b42fc393614
amagramming-git <knb1031_ba@icloud.com>
2023-12-09 20:36:39 +0900

    追加 初期画面コミット

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+    return (
+        <>
+            <div>
+                <h1>こんにちは</h1>
+            </div>
+        </>
+    )
+}
